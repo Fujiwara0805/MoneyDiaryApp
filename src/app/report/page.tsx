5df4051d221321b5b14b9c8e3799feacaff593d2
Fujiwara0805 <tiki4091@gmail.com>
2024-03-07 08:16:14 +0900

    🚧 Report画面の実装①

---
+++ src/app/report/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const Report = () => {
+  return (
+    <main>
+      <div>report</div>
+      <Link href={"/"}>home</Link>
+    </main>
+  );
+};
+
+export default Report;

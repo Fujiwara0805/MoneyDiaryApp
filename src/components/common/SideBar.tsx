6fd980ff1be27b226f40467044ee9a3dd4e7e43d
Fujiwara0805 <tiki4091@gmail.com>
2024-03-11 08:13:38 +0900

    ✨ SideBarコンポーネントを実装

---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import HomeIcon from "@mui/icons-material/Home";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import { useRouter } from "next/navigation";
+
+interface SideBarProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  handleDrawerClose: () => void;
+  handleDrawerTransitionEnd: () => void;
+}
+
+interface MenuItem {
+  theme: string;
+  path: string;
+  icon: React.ComponentType;
+}
+
+export const SideBar = ({
+  drawerWidth,
+  mobileOpen,
+  handleDrawerClose,
+  handleDrawerTransitionEnd,
+}: SideBarProps) => {
+  const router = useRouter();
+
+  const menuItems: MenuItem[] = [
+    {
+      theme: "HOME",
+      path: "/",
+      icon: HomeIcon,
+    },
+    {
+      theme: "REPORT",
+      path: "/report",
+      icon: SignalCellularAltIcon,
+    },
+  ];
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {menuItems.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                <item.icon />
+              </ListItemIcon>
+              <ListItemText primary={item.theme} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
+
+  return (
+    <>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </>
+  );
+};

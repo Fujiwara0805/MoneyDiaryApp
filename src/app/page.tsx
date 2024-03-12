22ef5fa0493b95e706b42c5d9d17c838be1e56f8
Fujiwara0805 <tiki4091@gmail.com>
2024-03-12 21:48:18 +0900

    💄 Home画面にコンポーネントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import Box from "@mui/material/Box";
 import AppLayout from "@/components/layouts/AppLayout";
+import MonthlySummary from "@/components/layouts/MonthlySummary";
+import Calendar from "@/components/layouts/Calendar";
+import TransactionMenu from "@/components/layouts/TransactionMenu";
+import TransactionForm from "@/components/layouts/TransactionForm";
 
 export default function Home() {
   return (
-    <main>
+    <main className=" bg-slate-300 min-h-screen">
       <AppLayout />
+      <Box className="flex ">
+        {/*左側コンテンツ*/}
+        <Box className=" flex-grow ">
+          <MonthlySummary />
+          <Calendar />
+        </Box>
+
+        {/*右側コンテンツ*/}
+        <Box>
+          <TransactionMenu />
+          <TransactionForm />
+        </Box>
+      </Box>
     </main>
   );
 }

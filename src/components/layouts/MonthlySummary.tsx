b386339beb00312d8e1f6ebb25873c8e04e4380e
Fujiwara0805 <tiki4091@gmail.com>
2024-03-13 08:17:25 +0900

    ✨ MonthlySummaryコンポーネントの実装

--- src/components/layouts/MonthlySummary.tsx
+++ src/components/layouts/MonthlySummary.tsx
@@ -1,18 +1,77 @@
 import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import React from "react";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
 const MonthlySummary = () => {
   return (
-    <Grid container>
-      <Grid item>
-        <Card>
-          <CardContent>
-            <Stack>
-              <ArrowUpwardIcon />
+    <Grid container spacing={{ xs: 1, sm: 2 }} mb={2}>
+      {/* 収入 */}
+      <Grid item xs={4} display={"flex"}>
+        <Card sx={{ bgcolor: "blue", color: "white", borderRadius: "10px" }}>
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowUpwardIcon sx={{ fontSize: "2rem" }} />
               <Typography>収入</Typography>
             </Stack>
-            <Typography>¥300</Typography>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"bold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: ".1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* 支出 */}
+      <Grid item xs={4} display={"flex"}>
+        <Card sx={{ bgcolor: "red", color: "white", borderRadius: "10px" }}>
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowDownwardIcon sx={{ fontSize: "2rem" }} />
+              <Typography>支出</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"bold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: ".1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* 残高 */}
+      <Grid item xs={4} display={"flex"}>
+        <Card sx={{ bgcolor: "green", color: "white", borderRadius: "10px" }}>
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <AccountBalanceIcon sx={{ fontSize: "2rem" }} />
+              <Typography>残高</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"bold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: ".1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
           </CardContent>
         </Card>
       </Grid>

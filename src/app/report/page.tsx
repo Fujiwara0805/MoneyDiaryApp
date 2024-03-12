16c4292c278d40c7393eabeeaa64e138f4933f2b
Fujiwara0805 <tiki4091@gmail.com>
2024-03-12 21:49:34 +0900

    🚧 Report画面の一部コードを修正

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
+import AppLayout from "@/components/layouts/AppLayout";
 import React from "react";
 
 const Report = () => {
   return (
-    <main>
-      <div>report</div>
-      <Link href={"/"}>home</Link>
+    <main className=" bg-slate-300 min-h-screen">
+      <AppLayout />
+      <div className=" text-center justify-center mr-8">
+        <h1>REPORT</h1>
+      </div>
     </main>
   );
 };

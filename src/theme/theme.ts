cc336cb4c59c87c19bed16fc63134e0b63e531a5
Fujiwara0805 <tiki4091@gmail.com>
2024-03-14 08:16:39 +0900

    🏷️ 型定義の削除

---
+++ src/theme/theme.ts
@@ -0,0 +1,40 @@
+// import { PaletteColor, PaletteColorOptions, createTheme } from "@mui/material";
+// import { blue, green, red } from "@mui/material/colors";
+
+// /* 型定義 */
+// declare module "@mui/material/styles" {
+//   interface Palette {
+//     incomeColor: PaletteColor;
+//     expenseColor: PaletteColor;
+//     balanceColor: PaletteColor;
+//   }
+
+//   interface PaletteOptions {
+//     incomeColor?: PaletteColorOptions;
+//     expenseColor?: PaletteColorOptions;
+//     balanceColor?: PaletteColorOptions;
+//   }
+// }
+
+// export const theme = createTheme({
+//   palette: {
+//     /* 収入用 */
+//     incomeColor: {
+//       main: blue[500],
+//       light: blue[100],
+//       dark: blue[700],
+//     },
+//     /* 支出用 */
+//     expenseColor: {
+//       main: red[500],
+//       light: red[100],
+//       dark: red[700],
+//     },
+//     /* 残高用 */
+//     balanceColor: {
+//       main: green[500],
+//       light: green[100],
+//       dark: green[700],
+//     },
+//   },
+// });

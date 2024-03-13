9f5a929de560988fb434ceede7e9f34b51afa95c
Fujiwara0805 <tiki4091@gmail.com>
2024-03-14 08:16:15 +0900

    🚧 firebase導入に向けた準備

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,25 @@ import MonthlySummary from "@/components/layouts/MonthlySummary";
 import Calendar from "@/components/layouts/Calendar";
 import TransactionMenu from "@/components/layouts/TransactionMenu";
 import TransactionForm from "@/components/layouts/TransactionForm";
+import { useEffect, useState } from "react";
+import { Transaction } from "@/types/type";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/db/firebase";
 
 export default function Home() {
+  /* firebaseからデータ抽出 */
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  useEffect(() => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "Transactions"));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, "→", doc.data());
+      });
+    } catch (error) {
+      console.log("データベース接続に失敗しました", error);
+    }
+  }, []);
+
   return (
     <main className=" bg-slate-300 min-h-screen">
       <AppLayout />

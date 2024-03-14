cf5105cf0a9cad4c9c84ca393d240df6b0cf7f29
Fujiwara0805 <tiki4091@gmail.com>
2024-03-15 08:15:37 +0900

    👔 ✨ appコンポーネントを導入しビジネスロジックを記述した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,36 @@
-import Box from "@mui/material/Box";
-import AppLayout from "@/components/layouts/AppLayout";
-import MonthlySummary from "@/components/layouts/MonthlySummary";
-import Calendar from "@/components/layouts/Calendar";
-import TransactionMenu from "@/components/layouts/TransactionMenu";
-import TransactionForm from "@/components/layouts/TransactionForm";
+"use client";
 import { useEffect, useState } from "react";
 import { Transaction } from "@/types/type";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/db/firebase";
+import Home from "./home/page";
 
-export default function Home() {
-  /* firebaseからデータ抽出 */
+export default function App() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  /* firebaseからデータ抽出 */
   useEffect(() => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "Transactions"));
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, "→", doc.data());
-      });
-    } catch (error) {
-      console.log("データベース接続に失敗しました", error);
-    }
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        const transactionData = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          } as Transaction;
+        });
+        setTransactions(transactionData);
+        console.log(transactionData);
+      } catch (error) {
+        console.log("データベース接続に失敗しました", error);
+      }
+    };
+    fetchTransactions();
   }, []);
 
   return (
     <main className=" bg-slate-300 min-h-screen">
-      <AppLayout />
-      <Box className="flex ">
-        {/*左側コンテンツ*/}
-        <Box className=" flex-grow text-center ">
-          <MonthlySummary />
-          <Calendar />
-        </Box>
-
-        {/*右側コンテンツ*/}
-        <Box>
-          <TransactionMenu />
-          <TransactionForm />
-        </Box>
-      </Box>
+      <Home />
     </main>
   );
 }

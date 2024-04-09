2739462391b604cf6bb9a1a891d260368b971034
Fujiwara0805 <tiki4091@gmail.com>
2024-04-10 08:16:37 +0900

    🚧 Report画面のデザイン作成

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,4 +1,5 @@
 import AppLayout from "@/components/layouts/AppLayout";
+import { Grid, Paper } from "@mui/material";
 import React from "react";
 
 interface ReportProps {
@@ -6,11 +7,27 @@ interface ReportProps {
 }
 
 const Report = ({ switchView }: ReportProps) => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+  };
   return (
     <main className=" bg-slate-300 min-h-screen">
       <AppLayout switchView={switchView} />
-      <div className=" text-center justify-center mr-8">
-        <h1>REPORT</h1>
+      <div className=" mx-4 my-4">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            日付
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper sx={commonPaperStyle}>カテゴリグラフ</Paper>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Paper sx={commonPaperStyle}>棒グラフ</Paper>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            テーブル
+          </Grid>
+        </Grid>
       </div>
     </main>
   );
